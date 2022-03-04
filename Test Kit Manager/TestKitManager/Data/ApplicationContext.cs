318fc55f1cb5409e86d064eac357b5354c74ca0a
Chris Caskey <cdcaskey@users.noreply.github.com>
2022-03-04 19:42:31 +0000

    Create solution with EF

---
+++ Test Kit Manager/TestKitManager/Data/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestKitManager.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public string DbPath { get; }
+
+        public ApplicationContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "database.db");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+    }
+}
